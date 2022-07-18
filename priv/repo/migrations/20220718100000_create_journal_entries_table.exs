e50d082b366f10c5ef312d9dda067dc4f292d3b0
Stephen Lewis <gpg@stephenlewis.me>
2022-07-18 11:42:56 +0100

    Add user_id column to journal_entries table, reorder migrations

--- priv/repo/migrations/20220718100000_create_journal_entries_table.exs
+++ priv/repo/migrations/20220718100000_create_journal_entries_table.exs
@@ -5,12 +5,14 @@ defmodule Chico.Repo.Migrations.CreateJournalEntriesTable do
 
   def change do
     create table(@table_name) do
+      add :user_id, references(:users), on_delete: :delete_all
       add :date, :date
       add :check_in, :text
       add :check_out, :text, null: true
       timestamps()
     end
 
+    create index(@table_name, [:user_id])
     create unique_index(@table_name, [:date])
   end
 end
