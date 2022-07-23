f36e0ebc4ea539cf7a17bae9d35bc4cba6c95787
Stephen Lewis <gpg@stephenlewis.me>
2022-07-23 14:35:09 +0100

    Add user factory; add user to journal entry factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,11 +5,22 @@ defmodule Chico.Factory do
     %Chico.Journals.JournalEntry{
       check_in: sequence(:check_in, &"Top of the morning #{&1}"),
       check_out: sequence(:check_out, &"End of the evening #{&1}"),
-      date: Date.add(Date.utc_today(), Enum.random(-100..0))
+      date: Date.add(Date.utc_today(), Enum.random(-100..0)),
+      user: build(:user)
     }
   end
 
   def open_journal_entry_factory do
     struct!(journal_entry_factory(), %{check_out: nil})
+  end
+
+  def user_factory do
+    password = "i-am-so-secure"
+
+    %Chico.Accounts.User{
+      email: sequence(:email, &"john.doe.#{&1}@example.com"),
+      hashed_password: Bcrypt.hash_pwd_salt(password),
+      password: password
+    }
   end
 end
